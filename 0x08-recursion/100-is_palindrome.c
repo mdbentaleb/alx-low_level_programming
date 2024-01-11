812947128dd8cac46bfa7634e2e7d3d3db0245a4
mdbentaleb <bent4leb@gmail.com>
2024-01-11 15:44:43 +0100

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,30 +4,49 @@
 * Return: int
 */
 
+int ft_strlen(char *s);
+int is_palindrome_recursive(char *s, int i, int j);
+
+
 int is_palindrome(char *s)
 {
-int i = 0;
-int len = 0;
-int j;
-int check = 1;
-
-while (s[len] != '\0')
-{
-len++;
+int len = ft_strlen(s);
+return (is_palindrome_recursive(s, 0, len - 1));
 }
 
-j = len - 1;
+/**
+* ft_strlen - calculates the string length
+* @s: pointer to string
+* Return: int
+*/
 
-while (i < j)
+int ft_strlen(char *s)
 {
+int len = 0;
+if (*s != '\0')
+{
+len = len + ft_strlen(s + 1) + 1;
+}
+return (len);
+}
+
+/**
+* is_palindrome_recursive - calculates the string length
+* @s: pointer to string
+* @i: int
+* @j: int
+* Return: int
+*/
+
+int is_palindrome_recursive(char *s, int i, int j)
+{
+if (i >= j)
+{
+return (1);
+}
 if (s[i] != s[j])
 {
-check = 0;
-break;
+return (0);
 }
-i++;
-j--;
-}
-
-return (check);
+return (is_palindrome_recursive(s, i + 1, j - 1));
 }
