e685091c9d855312d80fedc585b77dd5f55d413e
mdbentaleb <bentalebm@outlook.com>
2024-01-11 02:10:38 +0100

    update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* is_prime_number - Verify whether n is a prime number
+* @n: int
+* @n2: square root
+* Return: int
+*/
+
+int ft_check_prime(int n, int n2);
+int is_prime_number(int n)
+{
+return (ft_check_prime(n, 2));
+}
+
+/**
+* ft_check_prime - verifies all numbers smaller than n and can divid it
+* @n: int
+* @n2: int
+* Return: int
+*/
+
+int ft_check_prime(int n, int n2)
+{
+if (n2 >= n && n >= 2)
+{
+return (1);
+}
+else if (n <= 1 || n % n2 == 0)
+{
+return (0);
+}
+else
+{
+return (ft_check_prime(n, n2 + 1));
+}
+}
