e2b37e4acfe169b86c1c18b148ebcfbf80b55bb2
mdbentaleb <bent4leb@gmail.com>
2024-01-12 19:47:15 +0100

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,30 +1,6 @@
 #include <stdio.h>
 
 /**
- * _atoi - converts a string to an integrs
- *
- * @s: string input parameter
- *
- * Return: converted integer from string
- */
-
-int _atoi(char *s)
-{
-	unsigned int num = 0;
-	int sign = 1;
-
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-		else if (num > 0)
-			break;
-	} while (*s++);
-	return (num * sign);
-}
-
-/**
  * main - program that multiplies two numbers.
  * @arc: int
  * @arv: array
@@ -37,14 +13,14 @@ int main(int arc, char **arv)
 
 	if (arc == 3)
 	{
-		sum = _atoi(arv[1]) * _atoi(arv[2]);
-		printf("%d\n", sum);
+		sum = atoi(arv[1]) * atoi(arv[2]);
+		printf("%d", sum);
 	}
 	else
 	{
-		printf("Eroor\n");
-		return (1);
+		printf("Eroor");
 	}
+	printf("\n");
 	return (0);
 }
 
