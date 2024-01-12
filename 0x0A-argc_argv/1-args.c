4ba6a74e281515b5c2ed41a1a27784e3bd91c8c4
mdbentaleb <bentalebm@outlook.com>
2024-01-12 18:03:45 +0100

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints the number of arguments passed into it.
+ * a
+ * @argc: int
+ * @argv: array
+ * Return: 0;
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+}
