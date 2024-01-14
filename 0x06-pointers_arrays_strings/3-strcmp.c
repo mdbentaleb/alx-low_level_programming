8fb2f5265f1dafa816b3a11b2b93bbecee14115c
mdbentaleb <bent4leb@gmail.com>
2024-01-14 13:26:58 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,9 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	while (s1[i] != '\0')
-	{
-		while (s2[j] != '\0')
-		{
-			if (s1[i] == s2[j])
-			{
-				return (0);
-			}
-			else if (s1[i] >= s2[j] || s1[i] <= s2[j])
-				return (s1[i] - s2[j]);
-			j++;
-		}
+	while (s1[i] == s2[i] && s1[i] != '\0')
 		i++;
-	}
+	return (s1[i] - s2[i]);
 }
 
