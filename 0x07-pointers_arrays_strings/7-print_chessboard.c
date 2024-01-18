134c08bb3d4faad362f8e2724afb2f9e2eea9b74
mdbentaleb <bent4leb@gmail.com>
2024-01-18 03:34:57 +0100

    update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	for (; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
