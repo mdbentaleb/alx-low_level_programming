499e6b17ebaeba25a5560c82e8f805749d5d0e04
mdbentaleb <bentalebm@outlook.com>
2024-01-18 02:26:48 +0100

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (src[i] && i < n)
-	{
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-		i++;
-	}
+
 	return (dest);
 }
 
