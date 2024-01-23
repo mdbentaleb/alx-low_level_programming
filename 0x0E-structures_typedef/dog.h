b645b8f4b00d9149a962a3517f6818686b33c801
mdbentaleb <bentalebm@outlook.com>
2024-01-23 16:10:25 +0100

    -

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+char *ft_strcpy(char *dest, char *src);
+int ft_strlen(char *s);
 
 #endif
