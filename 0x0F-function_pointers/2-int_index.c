78da463142baf9a982e94789238e87312405cb85
mdbentaleb <bentalebm@outlook.com>
2024-01-25 00:13:54 +0100

    -

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointer"
+
+/**
+ * int_index - a function that searches for an integer.
+ * @array: array
+ * @size: array size
+ * @cmp: pointer to function
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
+
