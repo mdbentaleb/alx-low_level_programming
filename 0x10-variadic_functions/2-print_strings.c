cdca5439416a2bb7898fb915b3578ea59938f8b5
mdbentaleb <bentalebm@outlook.com>
2024-01-26 02:01:14 +0100

    -

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,7 +9,7 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	int i;
+	unsigned int i;
 	char *str;
 	va_list str_list;
 
@@ -27,6 +27,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	printf("\n");
 
-	va_end(str);
+	va_end(str_list);
 }
 
