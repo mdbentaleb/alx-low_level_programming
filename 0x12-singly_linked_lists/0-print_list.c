57221301521e4267d490540610db3fd1ede6cbb6
mdbentaleb <bent4leb@gmail.com>
2024-02-02 20:29:48 +0100

    -

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,45 +1,24 @@
 #include "lists.h"
 
 /**
- * ft_strlen - calculates length of strings
- * @str: input string
- *
- * Return: int
- */
-
-int ft_strlen(char *str)
-{
-	int len = 0;
-
-	while (str[i] != NULL)
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
- * print_list - a function that prints all the elements of a list_t list
- * @h: pointer to linked list "first none"
- *
- * Return: size
- */
+* print_list - a function that prints all the elements of a list_t list
+* @h: Pointer to a linked list
+*
+* Return: Number of nodes
+*/
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t s = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
-			printf("[%d] %s\n", ft_strlen(h->len), h->str);
-			h = h->next;
-			count++;
-		}
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
 	return (count);
 }
-
