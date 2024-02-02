e29cca77caba6364d3fcfccb78ffd3f8a1aa2af5
mdbentaleb <bent4leb@gmail.com>
2024-02-02 21:49:51 +0100

    -

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+* display - executes before main
+*/
+void __attribute__((constructor)) display()
+{
+	printf("You're beat! and yet, you must allow,\nI bore
+		my house upon my back!\n");
+}
