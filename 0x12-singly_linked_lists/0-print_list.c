91762e504c510a1d8789226471dd03b66103a36b
mdbentaleb <bentalebm@outlook.com>
2024-02-02 19:08:45 +0100

    -

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
+		if (h->str == NULL)
+			printf("[0] (nil\n)");
+		else
+		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
