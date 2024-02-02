10ff834e60c1aa26c6aa401aabf23c9574fe3817
mdbentaleb <bentalebm@outlook.com>
2024-02-02 19:22:28 +0100

    -

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * ft_strlen - calculates length of strings
+ * @str: input string
+ *
+ * Return: int
+ */
+
+int ft_strlen(char *str)
+{
+	int len = 0;
+
+	while (str[i] != NULL)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * print_list - a function that prints all the elements of a list_t list
  * @h: pointer to linked list "first none"
  *
@@ -17,7 +35,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", ft_strlen(h->len), h->str);
 			h = h->next;
 			count++;
 		}
