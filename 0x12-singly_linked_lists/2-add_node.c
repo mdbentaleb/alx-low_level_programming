013bfeaa67cbd0e6737b8d0f7c1eea089798cd22
mdbentaleb <bentalebm@outlook.com>
2024-02-02 21:02:28 +0100

    -

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,7 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 
 	if (!head || !new)
-		retirn(NULL);
+		return (NULL);
 	if (str)
 	{
 		new->str = strdup(str);
