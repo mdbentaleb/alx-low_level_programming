60f0f35761774a31ceba5f12bbc224bbadc60e67
mdbentaleb <bent4leb@gmail.com>
2024-02-02 21:00:06 +0100

    -

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * ft_strlen - calculates length os string
+ * @str: input string
+ *
+ * Return: string length
+ */
+
+int ft_strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* add_node - a function that adds a new node
+*	at the beginning of a list_t list.
+* @head: Pointer to pointer to linked list
+* @str: string
+*
+* Return: the address of the new element, or NULL if it failed
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (!head || !new)
+		retirn(NULL);
+	if (str)
+	{
+		new->str = strdup(str);
+		if (!new->str)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->len = ft_strlen(new->str);
+	}
+
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
