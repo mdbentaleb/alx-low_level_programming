a28e3f8b71a804877b7c76c265da6c1d887e225e
mdbentaleb <bent4leb@gmail.com>
2024-02-08 22:48:57 +0100

    -

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,9 @@
 
 #include <limits.h>
 #include <stdio.h>
+#include <unistd.h>
 
+void _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
