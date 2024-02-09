2aa9a158811786ac14b72847597efe0985c46279
mdbentaleb <bentalebm@outlook.com>
2024-02-09 14:46:15 +0100

    -

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - a function that checks the endianness.
+ * Return: int
+ */
+
+int get_endianness(void)
+{
+	unsigned long int nb = 1;
+
+	return (*(char)&nb);
+}
+
