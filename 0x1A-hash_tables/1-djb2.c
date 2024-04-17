bdbb320bbe63c67d07b74596954a33902d15724f
mdbentaleb <bentalebm@outlook.com>
2024-04-17 15:50:28 +0100

    -

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,18 +2,18 @@
 
 /**
  * hash_djb2 - a hash function implementing the djb2 algorithm.
- * @s: string
+ * @str: string
  * Return: hash value
  */
-unsigned long int hash_djb2(const unsigned char *s)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int e;
 
 	hash = 5381;
-	while ((e = *s++))
+	while ((e = *str++))
 	{
-		hash = ((hash << 5) + hash) + k;
+		hash = ((hash << 5) + hash) + e;
 	}
 	return (hash);
 }
